759aa948992370a15ef21c333dbd0916fa9633de
Guoguang <csg999@163.com>
2021-05-02 15:59:33 +0800

    Update me.vue & add loading page.

---
+++ components/meLoading.vue
@@ -0,0 +1,56 @@
+<template>
+	<view>
+		<image class="boxImage" src="https://6d65-meet-the-world-2g7kshiy287c49fe-1305360411.tcb.qcloud.la/static/image/box.png" mode="aspectFill"></image>
+		<view class="msg">
+			{{msg}}
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"meLoading",
+		data() {
+			return {
+				msg:'Loading Data...'
+			};
+		}
+	}
+</script>
+
+<style scoped> 
+	@keyframes shake{
+	    0% { transform: rotate(0deg); }
+	   10% { transform: rotate(-10deg); }
+	   20% { transform: rotate(0deg); }
+	   30% { transform: rotate(10deg); }
+	   40% { transform: rotate(0deg); }
+	   50% { transform: rotate(-10deg); }
+	   60% { transform: rotate(0deg); }
+	   70% { transform: rotate(10deg); }
+	   80% { transform: rotate(0deg); }
+	   90% { transform: rotate(-10deg); }
+	   100% { transform: rotate(0deg); }
+	}
+	.boxImage{
+		width: 200upx;
+		height: 200upx;
+		position: absolute;
+		left: 50%;
+		margin-left: -100upx;
+		top: 30%;
+		animation: shake 0.3s linear infinite;
+	}
+	.msg{
+		width: 600upx;
+		height: 100upx;
+		position: absolute;
+		left: 50%;
+		top: 55%;
+		margin-top: -50upx;
+		margin-left: -300upx;
+		font-size: 50upx;
+		font-weight: 400;
+		text-align: center;
+	}
+</style>
--- pages/me/me.vue
+++ pages/me/me.vue
@@ -1,31 +1,98 @@
 <template>
-	<view>me
+	<view>
+		<view v-if="loadingFinished">
+			<van-steps
+			  steps="{{ content }}"
+			  active="{{ active }}"
+			  direction="vertical"
+			  active-color="#000000"
+			  inactive-color="#000000"
+			  active-icon="clock"
+			  inactive-icon="clock"
+			  custom-class="step-class"
+			  desc-class="step-desc-class"
+			/>
+		</view>
+		<view v-else><me-loading></me-loading></view>
 		<my-tabbar now=1></my-tabbar>
 	</view>
 </template>
 
 <script>
 	import myTabbar from '../../components/myTabbar.vue'
+	import meLoading from '../../components/meLoading.vue'
 	export default {
 		components: {
-			myTabbar
+			myTabbar,
+			meLoading
 		},
 		data() {
 			return {
-
+				actice: 1,
+				loadingFinished: false,
+				items: [
+					{
+						index:"1",
+						content:"123"
+					},
+					{
+						index:"2",
+						content:"345"
+					}
+				],
+				content: []
 			}
 		},
-		onLoad() {
-
+		onShow() {
+			this['loadingFinished'] = false;
+			this['content']=[];
+			this.$options.methods.getSelfMessages(this);
+			console.log(this.$options);
 		},
 		methods: {
-
+			getSelfMessages(that){
+				wx.cloud.callFunction({
+					name:'pull_self',
+					data: {},
+					success:function(res){
+						console.log('[getSelfMessages] ok',res);
+						if(res.result.errCode == 13600){
+							that['loadingFinished'] = true;
+							that.$options.methods.updateData(that,res.result.data);
+						}
+					},
+					fail:function(err){
+						console.log('[getSelfMessages] failed',err);
+					}
+				});
+			},
+			updateData(that,resultData) {
+				console.log(resultData);
+				console.log(resultData.length);
+				for (let id in resultData) {
+					that['content'].push({
+						text:resultData[id].cre_time.replace("T"," ").substr(0,19),
+						desc:resultData[id].note_words,
+						pics:resultData[id].note_pic
+					});
+					console.log(that['content']);
+				}
+				
+			}
 		}
 	}
 </script>
 <style>
 
 </style>
-<style scoped>
-
+<style>
+	.step-class {
+		overflow: scroll !important;
+		max-height: 980rpx;
+		overflow-y: auto;
+	}
+	.step-desc-class {
+		font-size: 48rpx;
+		margin-bottom: 25rpx;
+	}
 </style>
